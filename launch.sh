090d6aa713bb5c06e5ccb0b78daa90506df4bf64
Erwann Roussy <erwann.roussy@savoirfairelinux.com>
2023-01-30 14:53:25 +0100

    launch.sh: separate code in three function
    Create configuration, setup_debian and launch_test function.
    The behavior is the same for now, but these function will be useful to
    separate task in the CI Github Action.
    
    Signed-off-by: Erwann Roussy <erwann.roussy@savoirfairelinux.com>

--- launch.sh
+++ launch.sh
@@ -21,75 +21,90 @@ USAGE:
 EOF
 }
 
-if test "$#" -ne 2; then
-  usage
-  die "invalid usage"
-fi
+# Configure and prepare the CI
+configuration() {
+  if test "$#" -ne 2; then
+    usage
+    die "invalid usage"
+  fi
 
-# Prerequisite
-if ! type -p cqfd > /dev/null; then
-  die "cqfd not found"
-fi
+  # Prerequisite
+  if ! type -p cqfd > /dev/null; then
+    die "cqfd not found"
+  fi
 
-PR_BRANCH=$1
-PR_HASH=`echo $2 | cut -b 1-7`
-CI_DIR=`pwd`
-PR_N=`echo $PR_BRANCH | cut -d '/' -f 3`
-TIME=`date +%Hh%Mm%S`
-CUKINIA_TEST_DIR=$CI_DIR/cukinia_tests
-REPORT_NAME=test-report_${PR_HASH}_${TIME}.pdf
-REPORT_DIR=$CI_DIR/reports/docs/reports/PR-${PR_N}
+  PR_BRANCH=$1
+  PR_HASH=`echo $2 | cut -b 1-7`
+  CI_DIR=`pwd`
+  PR_N=`echo $PR_BRANCH | cut -d '/' -f 3`
+  TIME=`date +%Hh%Mm%S`
+  CUKINIA_TEST_DIR=$CI_DIR/cukinia_tests
+  REPORT_NAME=test-report_${PR_HASH}_${TIME}.pdf
+  REPORT_DIR=$CI_DIR/reports/docs/reports/PR-${PR_N}
 
-# Get sources
-git clone -q https://github.com/seapath/ansible
-cd ansible
-git fetch -q origin ${PR_BRANCH}
-git checkout -q FETCH_HEAD
+  # Get sources
+  git clone -q https://github.com/seapath/ansible
+  cd ansible
+  git fetch -q origin ${PR_BRANCH}
+  git checkout -q FETCH_HEAD
 
-# Launch tests
-cqfd init
-cqfd -b prepare
-LOCAL_ANSIBLE_DIR=/home/virtu/ansible # Local dir that contains keys and inventories
-CQFD_EXTRA_RUN_ARGS="-v $LOCAL_ANSIBLE_DIR:/tmp" cqfd run ansible-playbook \
--i /tmp/seapath_inventories/seapath_cluster_ci.yml \
--i /tmp/seapath_inventories/seapath_ovs_ci.yml \
---key-file /tmp/ci_rsa --skip-tags "package-install" \
-playbooks/ci_the_one_playbook.yaml
+  cqfd init
+  cqfd -b prepare
+}
 
-# Create report
-mkdir $CUKINIA_TEST_DIR
-mv $CI_DIR/ansible/*.xml $CUKINIA_TEST_DIR
-cd $CI_DIR/ci/report-generator
-cqfd -q init
-if ! CQFD_EXTRA_RUN_ARGS="-v $CUKINIA_TEST_DIR:/tmp/cukinia-res" cqfd -q run; then
-  die "cqfd error"
-fi
+# Launch setup and hardening Debian
+setup_debian() {
+  LOCAL_ANSIBLE_DIR=/home/virtu/ansible # Local dir that contains keys and inventories
+  CQFD_EXTRA_RUN_ARGS="-v $LOCAL_ANSIBLE_DIR:/tmp" cqfd run ansible-playbook \
+  -i /tmp/seapath_inventories/seapath_cluster_ci.yml \
+  -i /tmp/seapath_inventories/seapath_ovs_ci.yml \
+  --key-file /tmp/ci_rsa --skip-tags "package-install" \
+  playbooks/ci_the_one_playbook.yaml
+}
 
-# Upload report
+# Prepare, launch test and upload test report
+launch_test() {
+  # TODO : split playbooks to call tests here
 
-git clone -q --depth 1 -b reports git@github.com:seapath/ci.git \
---config core.sshCommand="ssh -i ~/.ssh/ci_rsa" $CI_DIR/reports
-mkdir -p $REPORT_DIR
-mv $CI_DIR/ci/report-generator/main.pdf $REPORT_DIR/$REPORT_NAME
-cd $REPORT_DIR
-git config --local user.email "ci.seapath@gmail.com"
-git config --local user.name "Seapath CI"
-git config --local core.sshCommand "ssh -i ~/.ssh/ci_rsa"
-git add $REPORT_NAME
-git commit -q -m "upload report $REPORT_NAME"
-git push -q origin reports
+  # Create report
+  mkdir $CUKINIA_TEST_DIR
+  mv $CI_DIR/ansible/*.xml $CUKINIA_TEST_DIR
+  cd $CI_DIR/ci/report-generator
+  cqfd -q init
+  if ! CQFD_EXTRA_RUN_ARGS="-v $CUKINIA_TEST_DIR:/tmp/cukinia-res" cqfd -q run; then
+    die "cqfd error"
+  fi
 
-# Give link
-echo See test Report at \
-https://github.com/seapath/ci/blob/reports/docs/reports/PR-${PR_N}/${REPORT_NAME}
+  # Upload report
 
-# grep for succes
-if grep -q "<failure" $CUKINIA_TEST_DIR/*; then
-  RES=1
-else
-  RES=0
-fi
+  git clone -q --depth 1 -b reports git@github.com:seapath/ci.git \
+  --config core.sshCommand="ssh -i ~/.ssh/ci_rsa" $CI_DIR/reports
+  mkdir -p $REPORT_DIR
+  mv $CI_DIR/ci/report-generator/main.pdf $REPORT_DIR/$REPORT_NAME
+  cd $REPORT_DIR
+  git config --local user.email "ci.seapath@gmail.com"
+  git config --local user.name "Seapath CI"
+  git config --local core.sshCommand "ssh -i ~/.ssh/ci_rsa"
+  git add $REPORT_NAME
+  git commit -q -m "upload report $REPORT_NAME"
+  git push -q origin reports
 
-# remove github clone dir and cukinia test dir
-rm -rf $CI_DIR
-exit $RES
+  # Give link
+  echo See test Report at \
+  https://github.com/seapath/ci/blob/reports/docs/reports/PR-${PR_N}/${REPORT_NAME}
+
+  # grep for succes
+  if grep -q "<failure" $CUKINIA_TEST_DIR/*; then
+    RES=1
+  else
+    RES=0
+  fi
+
+  # remove github clone dir and cukinia test dir
+  rm -rf $CI_DIR
+  exit $RES
+}
+
+configuration
+setup
+test
