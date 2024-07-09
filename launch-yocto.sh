bb203874b78511fbe7c2ae2f526c54ef31f3e32b
Erwann Roussy <erwann.roussy@savoirfairelinux.com>
2024-07-09 13:28:28 +0200

    openlab/report.py: make compliance matrix link clickable
    A click on the compliance matrix tests will now jump to the test.
    This commit also adds a `-x` option to report.py
    
    It allows to specify the xml file to use. The compliance matrix are now
    created only for YoctoCI and not the guest machines.
    
    Signed-off-by: Erwann Roussy <erwann.roussy@savoirfairelinux.com>

--- launch-yocto.sh
+++ launch-yocto.sh
@@ -258,13 +258,22 @@ generate_report() {
   # Change contact mailing list to seapath SFL mailing list
   sed -i 's/contact@savoirfairelinux/seapath@savoirfairelinux/g' test-report.adoc
 
-  # Generate test parts
+  # Generate Yocto CI tests part
   cqfd -q init
   if ! CQFD_EXTRA_RUN_ARGS="" cqfd -q run ./report.py \
           -m -i include \
+          -x include/cukinia_yoctoCI.xml \
           -c include/ANSSI-BP28-M-Recommendations.csv \
           -c include/ANSSI-BP28-MI-Recommendations.csv \
           -c include/ANSSI-BP28-MIE-Recommendations.csv; then
+            die "cqfd error"
+  fi
+
+  # Generate VM tests part
+  if ! CQFD_EXTRA_RUN_ARGS="" cqfd -q run ./report.py \
+          -m -i include \
+          -x include/cukinia_guest0.xml \
+          -x include/cukinia_guest1.xml; then
             die "cqfd error"
   fi
 
